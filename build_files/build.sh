f277d504b6d2c7a848af8a86d43d2c919c496df1
Manfred Lotz <manfred.lotz@posteo.de>
2025-09-16 14:47:38 +0200

    add specific software we need

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,11 +10,25 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+#dnf5 install -y ghostty asusctl asusctl-rog-gui dovecot dovecot-pigeonhole opensmtpd snapper
+dnf5 install -y snapper dovecot dovecot-pigeonhole opensmtpd
 
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
+
+#  https://copr.fedorainfracloud.org/coprs/lukenukem/asus-linux/
+dnf5 copr -y enable lukenukem/asus-linux
+dnf5 install -y asusctl asusctl-rog-gui
+
+# wezterm-nightly: https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly
+dnf5 -y copr enable wezfurlong/wezterm-nightly
+dnf5 -y install wezterm
+
+# # _copr:copr.fedorainfracloud.org:scottames:ghostty.repo
+# dnf5 -y copr enable scottames/ghostty
+# dnf5 -y install ghostty
+
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
