e801aa39107c3f284dcec451334e0408f5410af9
Manfred Lotz <manfred.lotz@posteo.de>
2026-04-21 14:58:37 +0200

    again removing emacs as we want to install emaca via with systemd-sysext

--- build_files/build.sh
+++ build_files/build.sh
@@ -53,9 +53,6 @@ dnf5 remove -y fw-fanctrl
 # causes problems sometimes
 dnf5 remove -y input-remapper
 
-# install emacs
-dnf5 install -y emacs-pgtk
-
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
